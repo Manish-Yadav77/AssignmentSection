91373b4d10cb22cb7b78efd1a6d00488e139ed6f
Manish-Yadav77 <manishkyadav969@gmail.com>
2025-03-24 19:44:04 +0530

    Task Management Dashboard

--- Task Management Dashboard/src/components/TodoItem.jsx
+++ Task Management Dashboard/src/components/TodoItem.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useTodo } from "../context/TodoContext";
 
-function TodoItem({ todo}) {
-    const { toggleComplete,deleteTodo} = useTodo()
+function TodoItem({ todo }) {
+  const { toggleComplete, deleteTodo } = useTodo();
 
-    const toggleCompleted = () => {
-        toggleComplete(todo.id)
-    }
+  const toggleCompleted = () => {
+    toggleComplete(todo.id);
+  };
   return (
     <div
       className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
@@ -19,18 +19,20 @@ function TodoItem({ todo}) {
         checked={todo.completed}
         onChange={toggleCompleted}
       />
-      <input
-        type="text"
-        className={`border outline-none w-full bg-transparent rounded-lg ${todo.completed ? "line-through" : ""}`}
-        value={todo.todo}
-        readOnly={true}
-      />
-      <input
-        type="text"
-        className={`border outline-none w-full bg-transparent rounded-lg ${todo.completed ? "line-through" : ""}`}
-        value={todo.desc}
-        readOnly={true}
-      />
+      <p
+        className={`border outline-none w-full bg-transparent rounded-lg ${
+          todo.completed ? "line-through" : ""
+        }`}
+      >
+        {todo.todo}
+      </p>
+      <p
+        className={`border outline-none w-full bg-transparent rounded-lg ${
+          todo.completed ? "line-through" : ""
+        }`}
+      >
+        {todo.desc}
+      </p>
       {/* Delete Todo Button */}
       <button
         className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
